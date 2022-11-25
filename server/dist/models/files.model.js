601429f382cc4369e72cc30480cc58f29ebfdf60
riteshbehera857 <riteshbehera857@gmail.com>
2022-11-25 12:04:51 +0530

    another Build issue fixed

--- server/dist/models/files.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const fileSchema = new mongoose_1.Schema({
-    file: { type: String, required: true },
-});
-const File = (0, mongoose_1.model)("File", fileSchema);
-exports.default = File;
-//# sourceMappingURL=files.model.js.map
